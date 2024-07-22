2791ed580c695f00155eb3e18164ff8dc40922d6
randeepsingh4888 <162080653+randeepsingh4888@users.noreply.github.com>
2024-07-22 09:03:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,9 +29,9 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8
 CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
-STREAM_API = (environ.get('STREAM_API', 'ae0832f39e24094a0661626a792e6a2b8880e310'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/HowToOpenLinkHP/69'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'omegalinks.in'))
+STREAM_API = (environ.get('STREAM_API', 'e384968cfa6f14198e9bb3c61e269e00bb28dfd6'))
+STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/bdkdjhk/17'))
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5311861748').split()]
